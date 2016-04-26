3efc732d8a695fcc97caaf99d8c9499c649fea5d
mafeahumada <johahumada0108@hotmail.com>
2016-04-26 10:30:49 -0500

    reemplace
    reemplace guardar.html por guardar.php

--- guardar.php
+++ guardar.php
@@ -1,14 +1,10 @@
 <?php 
-$nom=$_POST["nom"];
-$ape=$_POST["ape"];
-$tdoc=$_POST["tdoc"];
-$ndoc=$_POST["ndoc"];
-$dir=$_POST["dir"];
-$barr=$_POST["barr"];
-$ciu=$_POST["ciu"];
+$nit=$_POST["nit"];
+$ras=$_POST["ras"];
+$dire=$_POST["dire"];
 $tel=$_POST["tel"];
-$cor=$_POST["cor"];
+$email=$_POST["cor"];
+
 $db=mysql_connect("localhost","root","");
-mysql_select_db("nuschi",$db);
-mysql_query("insert into acudiente values('$nom','$ape','$tdoc','$ndoc','$dir', '$barr','$ciu','$tel','$cor')");
-?>
+mysql_select_db("protsys",$db);
+
