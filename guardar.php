2c3e330dedad17ee7cd7a294f438aa0ed648c663
mafeahumada <johahumada0108@hotmail.com>
2016-04-26 11:34:23 -0500

    Guarde
    Guarde

--- guardar.php
+++ guardar.php
@@ -4,7 +4,6 @@ $ras=$_POST["ras"];
 $dire=$_POST["dire"];
 $tel=$_POST["tel"];
 $email=$_POST["cor"];
-
 $db=mysql_connect("localhost","root","");
 mysql_select_db("clientes",$db);
 mysql_query("insert into cliente values('$nit','$ras','$dire','$tel','$cor',)");
