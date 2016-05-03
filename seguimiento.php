9884042fdbff7339fc50a8bb996671e4eaad7a9a
mafeahumada <johahumada0108@hotmail.com>
2016-05-03 08:50:51 -0500

    cambios
    nuevo guardar1.php
    y modificacion de seguimiento.php

--- seguimiento.php
+++ seguimiento.php
@@ -110,7 +110,7 @@
 
         </div>
         <div class="col-md-6">
-          <center><h1> <strong>Formulario de Personas </strong></h1></center>
+          <center><h1> <strong>Formulario de seguimiento </strong></h1></center>
           <hr>
           <<!-- p>
             aqui se guarda una person
